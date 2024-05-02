f29c0e987c1b2cde6101734c825322ebc8f4ae7a
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-01 23:25:35 -0300

    Criando_Dificuldades
    Criei niveis de dificuldade.

--- main.cpp
+++ main.cpp
@@ -2,27 +2,35 @@
 
 using namespace std;
 
-bool chutes(short numSorte){
+bool chutes(short numSorte, short dificuldade){
   short numChute = 0;
-  short tentativas = 0;
+  short tentativas = 1;
+  short numD2 = 0;
+
+  if(dificuldade == 2) numD2 = 16;
+  else if(dificuldade == 3) numD2 = 6;
 
   do{
-    cout << "\n\tTentativa " << tentativas << " chute um numero: ";
+
+    cout << "\n\tTentativas " << tentativas - 1 << " chute um numero: ";
     cin >> numChute;
     tentativas++;
-  }while(numChute != numSorte);
+  }while(numChute != numSorte && tentativas != numD2);
 
   return (numChute == numSorte);
 }
 
 int main(){
   const short numSorte = 42;
+  short dificuldade = 0; 
 
   cout << "\t********************\n";
   cout << "\t*----Hello Word----*\n";
   cout << "\t********************\n";
 
-  const bool ganhou =  chutes(numSorte);
+  cout << "\n\tEscolha sua dificuldade \n facil: 1\n medio: 2\n dificil: 3\n\tdificuldade: ";
+  cin >> dificuldade;
 
-  cout << "\n\tSua vitoria é " << ganhou << "\n";
+  if(chutes(numSorte, dificuldade)) cout << "\n\tVocê ganhou, parabéns\n";
+  else cout << "\n\tVocê perdeu, quem sabe na proxima.\n";
 }
