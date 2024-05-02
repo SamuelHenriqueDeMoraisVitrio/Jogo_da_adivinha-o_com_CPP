f3c10a4d2de05a200fa68db715bd3a6ed8f63aa6
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-01 22:46:33 -0300

    Criando_função
    Criei uma função com o objetivo de deixar o codigo mais legivel, sendo que o tipo da função é bool.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,27 @@
 
 using namespace std;
 
-int main(){
+bool chutes(short numSorte){
   short numChute = 0;
+  short tentativas = 0;
+
+  do{
+    cout << "\n\tTentativa " << tentativas << " chute um numero: ";
+    cin >> numChute;
+    tentativas++;
+  }while(numChute != numSorte);
+
+  return (numChute == numSorte);
+}
+
+int main(){
+  const short numSorte = 42;
 
   cout << "\t********************\n";
   cout << "\t*----Hello Word----*\n";
   cout << "\t********************\n";
 
-  cout << "\n\tChute um numero: ";
-  cin >> numChute;
+  const bool ganhou =  chutes(numSorte);
+
+  cout << "\n\tSua vitoria é " << ganhou << "\n";
 }
