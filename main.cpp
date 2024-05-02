bdfd317a52ce51ff96fdd3ab61e96ff167370857
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-05-02 00:42:14 -0300

    Pontuação
    Criei uma pontuação

--- main.cpp
+++ main.cpp
@@ -2,22 +2,34 @@
 
 using namespace std;
 
-bool chutes(short numSorte, short dificuldade, short *tentativas){
+bool chutes(short numSorte, short dificuldade, short *tentativas, short *pontos){
   short numChute = 0;
   short numD2 = -1;
 
-  if(dificuldade == 2) numD2 = 16;
-  else if(dificuldade == 3) numD2 = 6;
+  if(dificuldade == 2){
+    numD2 = 16;
+    *pontos = 200;
+  }else if(dificuldade == 3){
+    numD2 = 6;
+    *pontos = 300;
+  }
 
   do{
 
     cout << "\n\tTentativas " << *tentativas << " chute um numero: ";
-    *tentativas = *tentativas + 1;
+
     cin >> numChute;
 
     if(numChute < numSorte)cout << "\tSeu numero é menor que o numero da sorte.\n";
     else if(numChute > numSorte) cout << "\tSeu numero é maior que o numero da sorte.\n";
 
+    if(*tentativas > 0 || numChute == numSorte){
+      if(dificuldade == 1) *pontos = *pontos - 2;
+      else if(dificuldade == 2) *pontos = *pontos - 14;
+      else if(dificuldade == 3) *pontos = *pontos - 60;
+    }
+
+    *tentativas = *tentativas + 1;
   }while(numChute != numSorte && *tentativas != numD2);
 
   return (numChute == numSorte);
@@ -27,6 +39,7 @@ int main(){
   const short numSorte = 42;
   short dificuldade = 0; 
   short tentativas = 0;
+  short pontos = 100;
 
   cout << "\t********************\n";
   cout << "\t*----Hello Word----*\n";
@@ -35,8 +48,11 @@ int main(){
   cout << "\n\tEscolha sua dificuldade \n facil: 1\n medio: 2\n dificil: 3\n\tdificuldade: ";
   cin >> dificuldade;
 
-  if(chutes(numSorte, dificuldade, &tentativas)) cout << "\n\tVocê ganhou, parabéns\n";
-  else cout << "\n\tVocê perdeu, quem sabe na proxima.\n";
+  if(chutes(numSorte, dificuldade, &tentativas, &pontos)) cout << "\n\tVocê ganhou, parabéns\n";
+  else{
+    cout << "\n\tVocê perdeu, quem sabe na proxima.\n";
+    pontos = 0;
+  }
 
-  cout << "\n\tSeu tentou " << tentativas << " vezes\n\n";
+  cout << "\n\tVocê tentou " << tentativas << " vezes\n\n\t" << pontos << " Pontos\n\n";
 }
